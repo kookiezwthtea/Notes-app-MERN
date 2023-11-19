37106db724c7ee0f42dff7e89e56dc7921ad0334
unknown <sahemaliii@gmail.com>
2023-11-19 21:41:13 +0200

    my commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import env from "./util/validateEnv";
+import mongoose from "mongoose";
+import app from "./app";
+
+
+const port = env.PORT;
+
+mongoose.connect(env.MONGO_CONNECTION_STRING)
+.then(()=>{
+    console.log("Mongoose connected");
+
+    app.listen(port, ()=>{
+        console.log(`Server running on port: ${port} `);
+    });
+})
+.catch(console.error);
+
