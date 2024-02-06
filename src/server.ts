c559e60197a94833bb34a0007b2e7ed65bd066c6
unknown <sahemaliii@gmail.com>
2024-02-06 02:34:54 +0200

    add cors

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,15 @@ const port = env.PORT;
 // Allow requests from a specific origin
 app.use(express.json());
 
+app.options('/api/users/login', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'https://notes-app-mern-lgpr.onrender.com');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.sendStatus(204); // No content in the response
+  });
+  
+
 // Middleware for handling CORS POLICY
 app.use(
   cors({
