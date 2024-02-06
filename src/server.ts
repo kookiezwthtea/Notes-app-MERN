d78cf920ff2bbf78d047bb6e9fa3b9b69bcc5bae
unknown <sahemaliii@gmail.com>
2024-02-06 02:05:18 +0200

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import app from "./app";
 import env from "./util/validateEnv";
 import mongoose from "mongoose";
+import cors from 'cors';
 
 const port = env.PORT;
+
+app.use(cors());
 
 mongoose.connect(env.MONGO_CONNECTION_STRING, {
   ssl: true,
