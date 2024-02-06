a4cc866ae7b55e69ac14bf6d6c496b1febf7d2d5
unknown <sahemaliii@gmail.com>
2024-02-06 03:14:19 +0200

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,8 @@
 import app from "./app";
 import env from "./util/validateEnv";
 import mongoose from "mongoose";
-import cors from 'cors';
-import express from "express";
 
 const port = env.PORT;
-
-
-// app.options('/',(req, res) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//   200  res.setHeader('Access-Control-Allow-Credentials', 'true');
-//     res.sendStatus(204);
-// });
-  
-const corsOpts = {
-    origin: '*',
-    methods: [
-      'GET',
-      'POST',
-    ],
-    allowedHeaders: [
-      'Content-Type',
-    ],
-  };
-  app.use(cors(corsOpts));
-
-  app.use(function(req, res, next) {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-      res.setHeader('Access-Control-Allow-Credentials', 'true');
-      next();
-  });
 
 
 mongoose.connect(env.MONGO_CONNECTION_STRING, {
